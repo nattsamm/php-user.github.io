3b4b0e29ad66f728b6a19ac7c2d79111d9fcc836
Natnael Samuel <55251383+nattsamm@users.noreply.github.com>
2021-08-17 04:11:55 +0300

    Enter

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 $hostname = "localhost";
 $username = "akhfqkli_login_register_youtube";
 $password = "_RxF9!NO?mzv";
-$database = "akhfqkli_login_register_youtube";
+$database = "reg";
 
 $conn = mysqli_connect($hostname, $username, $password, $database) or die("Database connection failed");
 
