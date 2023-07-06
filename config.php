249a1cf96d5197a876774934eafd38f7e888edda
Natnael Samuel <55251383+nattsamm@users.noreply.github.com>
2023-07-06 20:16:19 +0300

    Update config.php
    new

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ $user = "root";
 $pass = "";
 $database = "loginsystem";
 
-$conn = mysqli_connect($hostname, $username, $password, $database) or die("Database connection failed");
+$conn = mysqli_connect($server, $user, $pass, $database) or die("Database connection failed");
 
 $base_url = "https://code.akhfasoft.net/login-register-youtube/";
 $my_email = "code@akhfasoft.net";
